ff78762c9e87c0ab3e4cd1a1e3503c303f0d0d41
ethanhalpern <halpm171@newschool.edu>
2020-10-18 22:32:14 -0400

    new

---
+++ mnc/projects/one/index.js
@@ -0,0 +1,10 @@
+var source = "assets/mnc.mov"
+ var video = document.createElement("video");
+ //
+ video.autoplay = true;
+ //
+ video.load()
+ video.addEventListener("load", function() { 
+     video.play(); 
+ }, true);
+ video.src = source;
